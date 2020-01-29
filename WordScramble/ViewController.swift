0e21e3004716cb7f32b2a430063401a78b38bd22
Elena Kulakova <egeorgievska89@gmail.com>
2020-01-29 12:08:39 +0100

    Initial Commit

---
+++ WordScramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by Elena Kulakova on 2020-01-29.
+//  Copyright © 2020 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
